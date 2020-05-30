b2988c5b13700421612b351c425dfe2481b90156
Use your GitHub username <Enter your email address>
2020-05-29 21:33:47 -0600

    update

--- function_index.js
+++ function_index.js
@@ -5,3 +5,12 @@ document.getElementById('current-year').innerHTML = y;
 
 var update = document.lastModified;
 document.getElementById('update-time').innerHTML = update;
+
+WebFont.load({
+    google: {
+        families: [
+            'Alegreya Sans',
+            'Montserrat'
+        ]
+    }
+})
