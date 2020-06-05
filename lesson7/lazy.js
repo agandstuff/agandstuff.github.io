5a1a61324fc5a7881df59f11ce9a1474a3c73be0
Use your GitHub username <Enter your email address>
2020-06-04 19:52:12 -0600

    update

--- lesson7/lazy.js
+++ lesson7/lazy.js
@@ -1,7 +1,7 @@
 let imagesToLoad = document.querySelectorAll('img[data-src]');
 
 const imgOptions = {
-    threshold: 0;
+    threshold: 0,
     rootMargin: "0px 0px 50px 0px"
 };
 
@@ -19,6 +19,10 @@ imagesToLoad.forEach((img) => {
 if('IntersectionObserver' in window) {
     const observer = new IntersectionObserver((items, observer) => {
         items.forEach((item) => {
+            if(item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
         });
     }, imgOptions);
     imagesToLoad.forEach((img) => {
