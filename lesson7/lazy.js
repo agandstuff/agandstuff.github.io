6094c73ebc3da5699b29802f356755b646ff0732
Use your GitHub username <Enter your email address>
2020-06-04 21:48:13 -0600

    update

--- lesson7/lazy.js
+++ lesson7/lazy.js
@@ -11,11 +11,6 @@ imagesToLoad.forEach((img) => {
     loadImages(img);
 });
 
-const imgOptions = {
-    threshold: 0,
-    rootMargin: "0px 0px 50px 0px"
-};
-
 if('IntersectionObserver' in window) {
     const observer = new IntersectionObserver((items, observer) => {
         items.forEach((item) => {
@@ -24,7 +19,7 @@ if('IntersectionObserver' in window) {
                 observer.unobserve(item.target);
             }
         });
-    }, imgOptions);
+    });
     imagesToLoad.forEach((img) => {
         observer.observe(img);
     });
