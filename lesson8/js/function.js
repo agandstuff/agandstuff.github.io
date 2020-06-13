d02462716ac75d9f2d4b7ddc4697f62409c8db68
Use your GitHub username <Enter your email address>
2020-06-12 22:19:15 -0600

    update

---
+++ lesson8/js/function.js
@@ -0,0 +1,21 @@
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+
+const today = new Date();
+var weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'September', 'October', 'November', 'December'];
+document.getElementById('currentDate').innerHTML = weekday[today.getDay()]+', '+today.getDate()+' '+month[today.getMonth()]+' '+today.getFullYear();
+
+const dayNumber = today.getDay();
+const element = document.getElementById('message');
+if (dayNumber == 5) {
+    element.classList.add('showme');
+}
+else {
+    element.classList.add('hideme');
+}
+
+function changeRating(rating) {
+    document.getElementById('ratingValue').innerHTML = rating;
+}
